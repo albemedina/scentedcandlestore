0054c3ea92ebe728945373eda8ba648cce13a5f9
albemedina <medinaa.atc@gmail.com>
2021-04-15 00:56:11 -0700

    update

---
+++ css/js/add-content.js
@@ -0,0 +1,26 @@
+function welcomeHere(){
+    var today= new Date(); 
+    var hourNow = today.getHours(); 
+    var greeting; 
+    if (hourNow > 18) { 
+    greeting= 'Take a whiff with one of our imported candles and feel like your else where'; 
+    }
+    else if (hourNow > 12) { 
+    greeting = 'Where do you wanna go to nasally today?'; 
+    }
+    else if (hourNow >= 0) { 
+    greeting = 'In the early morning, light an amazing scent to start the new day!'; 
+    }
+    else { 
+    greeting = 'Welcome!'; 
+    }
+    return '<h3>' + greeting + '</h3>' ;   
+    }
+  function addUserName(){
+    var name = prompt ("What is your name?");
+    var greeting;
+    if (name !==""){
+        greeting = "Hello " + name;
+    } 
+    return '<h3>' + greeting + '</h3>';
+  }
